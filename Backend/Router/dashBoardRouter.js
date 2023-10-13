742d1f2d3d93182a23631f69d588b2c54304148d
ganesh <ganeshyadavstudies@gmail.com>
2023-10-14 03:36:38 +0530

    Added

--- Backend/Router/dashBoardRouter.js
+++ Backend/Router/dashBoardRouter.js
@@ -7,35 +7,68 @@ const dashboardRouter = express.Router();
 // Middleware to ensure authentication before accessing the dashboard
 dashboardRouter.use(authMiddleware);
 
-dashboardRouter.get('/', async (req, res) => {
+dashboardRouter.get('/', (req, res) => {
     const userID = req.body.userID; // Assuming the authenticated user's ID is available in the request body
 
-    try {
-        // Fetch user's tasks from the database
-        const tasksQuery = 'SELECT id, title, dueDate, status FROM tasks WHERE assignedTeamMembers LIKE ?';
-        const tasks = await db.query(tasksQuery, [`%${userID}%`]);
+    // Fetch user's tasks from the database
+    const tasksQuery = 'SELECT taskID, title, dueDate, status FROM tasks WHERE assignedTeamMembers LIKE ?';
+    db.query(tasksQuery, [`%${userID}%`], (tasksError, tasks) => {
+        if (tasksError) {
+            console.error(tasksError);
+            return res.status(500).json({ error: 'Internal Server Error 1' });
+        }
 
         // Fetch user's projects from the database
-        const projectsQuery = 'SELECT id, name, status FROM projects WHERE managerID = ?';
-        const projects = await db.query(projectsQuery, [userID]);
+        const projectsQuery = 'SELECT projectID, proName,description FROM projects WHERE managerID = ?';
+        db.query(projectsQuery, [userID], (projectsError, projects) => {
+            if (projectsError) {
+                console.error(projectsError);
+                return res.status(500).json({ error: 'Internal Server Error 2' });
+            }
 
-        // Fetch user's teams from the database
-        const teamsQuery = 'SELECT id, name FROM teams WHERE members LIKE ?';
-        const teams = await db.query(teamsQuery, [`%${userID}%`]);
+            // Fetch user's teams from the database
+            const teamsQuery = 'SELECT id, name FROM teams WHERE members LIKE ?';
+            db.query(teamsQuery, [`%${userID}%`], (teamsError, teams) => {
+                if (teamsError) {
+                    console.error(teamsError);
+                    return res.status(500).json({ error: 'Internal Server Error 3' });
+                }
 
-        // Prepare the response
-        const dashboardData = {
-            tasks: tasks,
-            projects: projects,
-            teams: teams
-        };
+                // Prepare the response
+                const dashboardData = {
+                    tasks: tasks,
+                    projects: projects,
+                    teams: teams
+                };
 
-        // Send the dashboard data as a JSON response
-        res.status(200).json(dashboardData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+                // Send the dashboard data as a JSON response
+                res.status(200).json(dashboardData);
+            });
+        });
+    });
+});
+
+dashboardRouter.get('/team-member/:userID', (req, res) => {
+    const userID = req.params.userID;
+
+    // Fetch tasks assigned to the team member
+    const tasksQuery1 = 'SELECT tasks.taskID AS taskID, tasks.title, tasks.dueDate, tasks.status FROM tasks JOIN task_team_members ON tasks.taskID = task_team_members.taskID WHERE task_team_members.teamMemberID = ?;';
+    db.query(tasksQuery1, [userID], (tasksError, tasks) => {
+        if (tasksError) {
+            console.error(tasksError);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          
+            const tasksQuery2 = 'SELECT tasks.taskID AS taskID, tasks.title, tasks.dueDate, tasks.status FROM tasks JOIN task_team_members ON tasks.taskID = task_team_members.taskID WHERE task_team_members.teamMemberID = ?;';
+            db.query(tasksQuery1, [userID], (tasksError, tasks) => {
+                if (tasksError) {
+                    console.error(tasksError);
+                    res.status(500).json({ error: 'Internal Server Error' });
+                }
+
+            res.status(200).json({ tasks: tasks });
+        }
+    });
 });
 
 module.exports = dashboardRouter;
--- Backend/Router/teamRouter.js
+++ Backend/Router/teamRouter.js
@@ -3,123 +3,193 @@ const db = require('../db'); // Assuming you have a database connection module
 
 const teamRouter = express.Router();
 
-teamRouter.post('/create', async (req, res) => {
-    const { name, members } = req.body;
+teamRouter.post('/create', (req, res) => {
+    const { name, members, projectID } = req.body;
 
     // Validate the request
     if (!name || name.trim() === '') {
         return res.status(400).json({ error: 'Team name is required.' });
     }
 
-    try {
-        // Create team record in the database
-        const createTeamQuery = 'INSERT INTO teams (name) VALUES (?)';
-        const [teamResult] = await db.query(createTeamQuery, [name]);
+    // Create team record in the database
+    const createTeamQuery = 'INSERT INTO teams (name, projectID) VALUES (?, ?)';
+    db.query(createTeamQuery, [name, projectID], (err, teamResult) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
 
         const teamID = teamResult.insertId;
+        const teamMembers = [];
 
         // Add team members if provided
         if (members && members.length > 0) {
-            const addMembersQuery = 'INSERT INTO teammembers (teamID, memberID) VALUES (?, ?)';
-            for (const memberID of members) {
-                // Check if memberID exists in users table (perform validation)
-                // ... (perform validation logic, if necessary)
+            const addMembersQuery = 'INSERT INTO teammembers (teamID, userID) VALUES (?, ?)';
+            let membersProcessed = 0;
 
-                // Insert team member record
-                await db.query(addMembersQuery, [teamID, memberID]);
-            }
+            members.forEach(memberID => {
+                db.query(addMembersQuery, [teamID, memberID], (err, memberResult) => {
+                    console.log(teamID,memberID)
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).json({ error: 'Internal Server Error 1' });
+                    }
+
+                    // Fetch member details from users table based on memberID
+                    const getMemberQuery = 'SELECT name, email FROM users WHERE id = ?';
+                    db.query(getMemberQuery, [memberID], (err, memberDetails) => {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).json({ error: 'Internal Server Error 2' });
+                        }
+
+                        if (memberDetails && memberDetails.length > 0) {
+                            const { name, email } = memberDetails[0];
+                            // Add member details to the teamMembers array
+                            teamMembers.push({ id: memberID, name, email });
+                        }
+
+                        membersProcessed++;
+
+                        // If all members are processed, send the response
+                        if (membersProcessed === members.length) {
+                            // Prepare response
+                            const teamResponse = {
+                                id: teamID.toString(),
+                                name,
+                                projectID,
+                                members: teamMembers
+                            };
+                            res.status(201).json(teamResponse);
+                        }
+                    });
+                });
+            });
+        } else {
+            // If there are no members, send the response immediately
+            const teamResponse = {
+                id: teamID.toString(),
+                name,
+                projectID,
+                members: teamMembers
+            };
+            res.status(201).json(teamResponse);
         }
-
-        // Prepare response
-        const teamResponse = {
-            id: teamID.toString(),
-            name,
-            members: members
-                ? members.map(memberID => {
-                      return {
-                          id: memberID,
-                          name: 'Sample Name', // Retrieve member details from users table
-                          email: 'sample@example.com' // Retrieve member details from users table
-                      };
-                  })
-                : []
-        };
-
-        res.status(201).json(teamResponse);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    });
 });
 
 
 
-teamRouter.put('/:teamID/members', async (req, res) => {
+
+teamRouter.put('update/:teamID', (req, res) => {
     const teamID = req.params.teamID;
-    const { members } = req.body;
+    const { members,projectID } = req.body;
 
     // Validate the request
-    if (!members || !Array.isArray(members) || members.length === 0) {
+    if (!projectID || !members || !Array.isArray(members) || members.length === 0) {
         return res.status(400).json({ error: 'Invalid or empty members array.' });
     }
 
-    try {
-        // Remove existing team members
-        const removeMembersQuery = 'DELETE FROM team_members WHERE teamID = ?';
-        await db.query(removeMembersQuery, [teamID]);
+    // Remove existing team members
+    const removeMembersQuery = 'DELETE FROM teammembers WHERE teamID = ?';
+    db.query(removeMembersQuery, [teamID], (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error 1' });
+        }
 
         // Add new team members
-        const addMembersQuery = 'INSERT INTO team_members (teamID, memberID) VALUES (?, ?)';
-        for (const memberID of members) {
+        const addMembersQuery = 'INSERT INTO teammembers (teamID, userID) VALUES (?, ?)';
+        let membersProcessed = 0;
+
+        members.forEach(memberID => {
             // Check if memberID exists in users table (perform validation)
             // ... (perform validation logic, if necessary)
 
             // Insert team member record
-            await db.query(addMembersQuery, [teamID, memberID]);
+            db.query(addMembersQuery, [teamID, memberID], (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Internal Server Error 2' });
+                }
+
+                membersProcessed++;
+
+                // If all members are processed, retrieve updated team details
+                if (membersProcessed === members.length) {
+                    const getTeamDetailsQuery =
+                    'SELECT teams.id AS teamID, teams.name AS teamName, teammembers.userID AS memberID, users.name AS memberName, users.email AS memberEmail FROM teams LEFT JOIN teammembers ON teams.id = teammembers.teamID LEFT JOIN users ON teammembers.userID = users.id WHERE teams.id = ?';
+                    
+                    db.query(getTeamDetailsQuery, [teamID], (err, teamDetailsResult) => {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).json({ error: 'Internal Server Error' });
+                        }
+
+                        // Prepare response
+                        const teamResponse = {
+                            id: teamID.toString(),
+                            name: teamDetailsResult[0].teamName,
+                            projectID,
+                            members: teamDetailsResult.map(member => {
+                                return {
+                                    id: member.memberID.toString(),
+                                    name: member.memberName,
+                                    email: member.memberEmail
+                                };
+                            })
+                        };
+
+                        res.status(200).json(teamResponse);
+                    });
+                }
+            });
+        });
+    });
+});
+
+
+teamRouter.delete('/delete/:teamID', (req, res) => {
+    const teamID = req.params.teamID;
+    const authenticatedUserID = req.body.userID; // Assuming you have the authenticated user ID in req.user
+
+    // Query to check if the authenticated user is the manager of the project associated with the team
+    const checkManagerQuery = 'SELECT projects.managerID FROM projects INNER JOIN teams ON projects.projectID = teams.projectID WHERE teams.id = ?';
+    
+    db.query(checkManagerQuery, [teamID], (error, results) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
 
-        // Retrieve updated team details with members
-        const getTeamDetailsQuery =
-            'SELECT teams.id AS teamID, teams.name AS teamName, users.id AS memberID, users.name AS memberName, users.email AS memberEmail FROM teams LEFT JOIN team_members ON teams.id = team_members.teamID LEFT JOIN users ON team_members.memberID = users.id WHERE teams.id = ?';
-        const [teamDetailsResult] = await db.query(getTeamDetailsQuery, [teamID]);
+        const project = results[0];
+        if (!project || project.managerID !== authenticatedUserID) {
+            return res.status(403).json({ error: 'Unauthorized access. Only managers can delete teams.' });
+        }
 
-        // Prepare response
-        const teamResponse = {
-            id: teamID.toString(),
-            name: teamDetailsResult[0].teamName,
-            members: teamDetailsResult.map(member => {
-                return {
-                    id: member.memberID.toString(),
-                    name: member.memberName,
-                    email: member.memberEmail
-                };
-            })
-        };
+        const deleteTeamMembersQuery = 'DELETE FROM teammembers WHERE teamID = ?';
+        db.query(deleteTeamMembersQuery, [teamID], (error) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
 
-        res.status(200).json(teamResponse);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+            // Delete the team from the teams table
+            const deleteTeamQuery = 'DELETE FROM teams WHERE id = ?';
+            db.query(deleteTeamQuery, [teamID], (error) => {
+                if (error) {
+                    console.error(error);
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
 
-teamRouter.delete('/:teamID', async (req, res) => {
-    const teamID = req.params.teamID;
-
-    try {
-        // Delete team members associated with the team
-        const deleteTeamMembersQuery = 'DELETE FROM team_members WHERE teamID = ?';
-        await db.query(deleteTeamMembersQuery, [teamID]);
-
-        // Delete the team from the teams table
-        const deleteTeamQuery = 'DELETE FROM teams WHERE id = ?';
-        await db.query(deleteTeamQuery, [teamID]);
+                // Send a success response
+                res.status(200).send('Team deleted successfully.');
+            });
+        });
 
         // Send a success response
-        res.status(200).send('Team deleted successfully.');
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+       
+    });
 });
+
 module.exports = teamRouter;
